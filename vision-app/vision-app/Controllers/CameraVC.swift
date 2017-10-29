3c95c6d4d1239285e9b2942329e3ab0418102e54
Benjamin Kimble <benjaminroykimble@gmail.com>
2017-10-29 14:26:50 -0600

    Added Camera Capture
    - Set up capture and display to cameraVC.

--- vision-app/vision-app/Controllers/CameraVC.swift
+++ vision-app/vision-app/Controllers/CameraVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
@@ -17,6 +18,9 @@ class CameraVC: UIViewController {
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var roundedLblView: RoundedShadowView!
     
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
     @IBAction func flashBtnPressed(_ sender: Any) {
     }
@@ -26,6 +30,38 @@ class CameraVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input) {
+                captureSession.addInput(input)
+            }
+            
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput) {
+                captureSession.addOutput(cameraOutput)
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+                previewLayer.connection?.videoOrientation = .portrait
+                
+                cameraView.layer.addSublayer(previewLayer)
+                captureSession.startRunning()
+            }
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+    }
 
 }
 
